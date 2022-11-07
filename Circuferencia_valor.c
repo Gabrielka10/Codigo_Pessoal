ec2799c03822ce880f62c9765bd0697c4b0f5d76
Gabrielka10 <gabrielcardosso10@gmail.com>
2022-11-07 03:09:57 -0300

    primeiro commit

---
+++ Circuferencia_valor.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int raio;
+    float  pi = 3.14;
+    float Circuferencia;
+    
+  printf("Digite o valor do raio:");
+  scanf("%i", &raio);
+  
+  
+  Circuferencia = 2 * pi * raio;
+  
+  printf("o valor da Circuferencia é = %f", Circuferencia);
+  
+    
+    return 0;
+}
+
